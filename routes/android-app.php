f409b5d0e9865292e419cad96e6fcecbe5730ded
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-18 01:13:42 +0330

    Add Android app download route

---
+++ routes/android-app.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\AndroidAppController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('download/android', AndroidAppController::class)
+    ->middleware('throttle:30,1')
+    ->name('android.download');
