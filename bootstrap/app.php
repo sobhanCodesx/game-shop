9708b79f84e03c14615e110b9e42c6d5dd01fa96
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-18 01:13:55 +0330

    Register Android app download routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
             Route::middleware('web')->group(base_path('routes/video-progress.php'));
             Route::middleware('web')->group(base_path('routes/video-preview.php'));
             Route::middleware('web')->group(base_path('routes/admin-file-manager.php'));
+            Route::middleware('web')->group(base_path('routes/android-app.php'));
         },
     )
     ->withMiddleware(function (Middleware $middleware): void {
