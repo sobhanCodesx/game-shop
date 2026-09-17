025da8ad9778113282c8192aa8fd8715325a1bba
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-17 23:07:16 +0330

    Load video preview routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
         then: function (): void {
             Route::middleware('web')->group(base_path('routes/video-progress.php'));
+            Route::middleware('web')->group(base_path('routes/video-preview.php'));
         },
     )
     ->withMiddleware(function (Middleware $middleware): void {
