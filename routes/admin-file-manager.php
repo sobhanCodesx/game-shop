06ad6fd5659be31182250d2146abfcb7b5f059b0
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-18 00:42:38 +0330

    Add admin file manager routes

---
+++ routes/admin-file-manager.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\Admin\ProjectFileManagerController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin/file-manager')
+    ->name('admin.file-manager.')
+    ->middleware(['auth', 'admin', 'super-admin', 'deployment.guard'])
+    ->controller(ProjectFileManagerController::class)
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::put('file', 'update')->name('file.update');
+        Route::post('file', 'storeFile')->name('file.store');
+        Route::post('directory', 'storeDirectory')->name('directory.store');
+        Route::post('upload', 'upload')->name('upload');
+        Route::patch('rename', 'rename')->name('rename');
+        Route::delete('entry', 'destroy')->name('destroy');
+        Route::get('download', 'download')->name('download');
+    });
