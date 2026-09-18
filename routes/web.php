e26d0ca7409fcdf4e5bfd8866cfe5ce30f8ca34b
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-18 14:05:11 +0330

    fix(explore): remove duplicate view route

--- routes/web.php
+++ routes/web.php
@@ -108,9 +108,6 @@ Route::get('products', [StorefrontController::class, 'shop'])->name('products.in
 Route::get('discover', [StorefrontController::class, 'discover'])->name('discover');
 Route::post('discover/content/{content:slug}/view', [StorefrontController::class, 'recordDiscoverView'])
     ->middleware('throttle:90,1')->name('discover.views.store');
-Route::post('discover/content/{content}/view', [StorefrontController::class, 'recordDiscoverView'])
-    ->middleware('throttle:120,1')
-    ->name('discover.content.view');
 Route::get('categories', [StorefrontController::class, 'shop'])->name('categories.index');
 Route::get('categories/{category:slug}', [StorefrontController::class, 'category'])->name('categories.show');
 Route::get('games', [StorefrontController::class, 'shop'])->name('games.index');
