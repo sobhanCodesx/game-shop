0dcd1c8dbc414e4b820027a4d3352cd9887e08b6
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-18 14:08:31 +0330

    style(explore): normalize view route chain

--- routes/web.php
+++ routes/web.php
@@ -107,7 +107,8 @@ Route::get('exchange-products', [StorefrontController::class, 'exchangeProducts'
 Route::get('products', [StorefrontController::class, 'shop'])->name('products.index');
 Route::get('discover', [StorefrontController::class, 'discover'])->name('discover');
 Route::post('discover/content/{content:slug}/view', [StorefrontController::class, 'recordDiscoverView'])
-    ->middleware('throttle:90,1')->name('discover.views.store');
+    ->middleware('throttle:90,1')
+    ->name('discover.views.store');
 Route::get('categories', [StorefrontController::class, 'shop'])->name('categories.index');
 Route::get('categories/{category:slug}', [StorefrontController::class, 'category'])->name('categories.show');
 Route::get('games', [StorefrontController::class, 'shop'])->name('games.index');
