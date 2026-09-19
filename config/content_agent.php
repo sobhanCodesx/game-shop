7b72a204eef8ad076fa965ad329ab15de85d1324
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-19 13:51:57 +0330

    feat(graph): configure safe AI query limits

--- config/content_agent.php
+++ config/content_agent.php
@@ -6,6 +6,17 @@ return [
     'allow_publish' => env('PLAYNEXUS_CONTENT_AGENT_ALLOW_PUBLISH', false),
     'allow_destructive' => env('PLAYNEXUS_CONTENT_AGENT_ALLOW_DESTRUCTIVE', false),
     'allow_uploads' => env('PLAYNEXUS_CONTENT_AGENT_ALLOW_UPLOADS', false),
+    'graphql' => [
+        'enabled' => env('PLAYNEXUS_CONTENT_AGENT_GRAPHQL_ENABLED', true),
+        'allow_introspection' => env('PLAYNEXUS_CONTENT_AGENT_GRAPHQL_INTROSPECTION', true),
+        'max_query_bytes' => env('PLAYNEXUS_CONTENT_AGENT_GRAPHQL_MAX_QUERY_BYTES', 24000),
+        'max_depth' => env('PLAYNEXUS_CONTENT_AGENT_GRAPHQL_MAX_DEPTH', 10),
+        'max_introspection_depth' => env('PLAYNEXUS_CONTENT_AGENT_GRAPHQL_MAX_INTROSPECTION_DEPTH', 16),
+        'max_complexity' => env('PLAYNEXUS_CONTENT_AGENT_GRAPHQL_MAX_COMPLEXITY', 500),
+        'max_fields' => env('PLAYNEXUS_CONTENT_AGENT_GRAPHQL_MAX_FIELDS', 250),
+        'max_page_size' => env('PLAYNEXUS_CONTENT_AGENT_GRAPHQL_MAX_PAGE_SIZE', 50),
+        'max_offset' => env('PLAYNEXUS_CONTENT_AGENT_GRAPHQL_MAX_OFFSET', 10000),
+    ],
     'uploads' => [
         'max_size' => env('PLAYNEXUS_CONTENT_AGENT_MAX_UPLOAD_SIZE', 104857600),
         'max_chunk_size' => env('PLAYNEXUS_CONTENT_AGENT_MAX_CHUNK_SIZE', 2097152),
