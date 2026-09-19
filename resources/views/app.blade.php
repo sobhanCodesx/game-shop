96570cd45158c5b960edc0b2ad96876903f3a1af
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-19 19:43:48 +0330

    ui: support mobile safe-area viewport

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html class="dark" data-theme="dark" lang="{{ str_replace('_', '-', config('seo.locale', 'fa-IR')) }}" dir="rtl">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="theme-color" content="#020307">
         <script>
