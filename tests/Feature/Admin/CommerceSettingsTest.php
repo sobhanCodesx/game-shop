fc0697bb6a49cc83e0021c1c41b2776e5e7e6c27
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-21 14:28:35 +0330

    update commerce admin tests for current RBAC model

--- tests/Feature/Admin/CommerceSettingsTest.php
+++ tests/Feature/Admin/CommerceSettingsTest.php
@@ -14,7 +14,7 @@ class CommerceSettingsTest extends TestCase
 
     public function test_commerce_update_preserves_home_settings_and_invalidates_home_cache(): void
     {
-        $admin = User::factory()->create(['is_admin' => true]);
+        $admin = User::factory()->create(['is_admin' => true, 'role' => 'super-admin']);
         HomeSetting::query()->create([
             'id' => 1,
             'content' => [
