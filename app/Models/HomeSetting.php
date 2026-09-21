dd92099e869cec91da32b30fc838582c8851fe45
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-21 14:28:09 +0330

    make home settings singleton id explicitly creatable

--- app/Models/HomeSetting.php
+++ app/Models/HomeSetting.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class HomeSetting extends Model
 {
-    protected $fillable = ['content'];
+    protected $fillable = ['id', 'content'];
 
     protected function casts(): array
     {
