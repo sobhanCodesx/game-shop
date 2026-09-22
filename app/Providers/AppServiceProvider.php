01ef83ee0a91d368d5a61eee58163b97f42f32ad
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-22 10:32:23 +0330

    Phase 6: register product cache dependency observers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,18 +2,26 @@
 
 namespace App\Providers;
 
+use App\Models\Brand;
 use App\Models\Category;
+use App\Models\CategoryAttribute;
 use App\Models\Game;
 use App\Models\HomeSlide;
+use App\Models\Platform;
 use App\Models\Product;
+use App\Models\ProductAttributeValue;
 use App\Models\ProductMedia;
 use App\Models\SocialContent;
 use App\Models\SocialContentMedia;
 use App\Models\Studio;
 use App\Models\VideoPlaylist;
+use App\Observers\BrandObserver;
+use App\Observers\CategoryAttributeObserver;
 use App\Observers\CategoryObserver;
 use App\Observers\GameObserver;
 use App\Observers\HomeSlideObserver;
+use App\Observers\PlatformObserver;
+use App\Observers\ProductAttributeValueObserver;
 use App\Observers\ProductMediaObserver;
 use App\Observers\ProductObserver;
 use App\Observers\SocialContentMediaObserver;
@@ -52,10 +60,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Brand::observe(BrandObserver::class);
         Category::observe(CategoryObserver::class);
+        CategoryAttribute::observe(CategoryAttributeObserver::class);
         Game::observe(GameObserver::class);
         HomeSlide::observe(HomeSlideObserver::class);
+        Platform::observe(PlatformObserver::class);
         Product::observe(ProductObserver::class);
+        ProductAttributeValue::observe(ProductAttributeValueObserver::class);
         ProductMedia::observe(ProductMediaObserver::class);
         SocialContent::observe(SocialContentObserver::class);
         SocialContentMedia::observe(SocialContentMediaObserver::class);
