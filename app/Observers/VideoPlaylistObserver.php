29669ff43feae57fa282b66be4354525663f044a
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-22 10:01:58 +0330

    Phase 1: invalidate playlist cache on collection changes

---
+++ app/Observers/VideoPlaylistObserver.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\VideoPlaylist;
+use App\Services\StorefrontPageCache;
+
+class VideoPlaylistObserver
+{
+    public function created(VideoPlaylist $playlist): void
+    {
+        $this->invalidate();
+    }
+
+    public function updated(VideoPlaylist $playlist): void
+    {
+        if ($playlist->wasChanged(['game_id', 'studio_id', 'title', 'slug', 'logo', 'description', 'visibility', 'sort_order'])) {
+            $this->invalidate();
+        }
+    }
+
+    public function deleted(VideoPlaylist $playlist): void
+    {
+        $this->invalidate();
+    }
+
+    private function invalidate(): void
+    {
+        app(StorefrontPageCache::class)->invalidate('playlist');
+    }
+}
