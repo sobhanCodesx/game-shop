d1c78bddba6d45ad1458f5e8593c08fc62f0c0a9
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-22 08:21:02 +0330

    feat(analytics): add Google Analytics tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="theme-color" content="#020307">
+
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6X384L0TH0"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-6X384L0TH0');
+        </script>
         <script>
             (() => {
                 let theme;
