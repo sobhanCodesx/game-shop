d00acae22efdda251eba1ccb232b48534f2579a8
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-22 10:35:36 +0330

    Phase 7: invalidate sitemap on studio changes

--- app/Observers/StudioObserver.php
+++ app/Observers/StudioObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Studio;
+use App\Services\SitemapCacheService;
 use App\Services\StorefrontPageCache;
 
 class StudioObserver
@@ -10,6 +11,7 @@ class StudioObserver
     public function created(Studio $studio): void
     {
         app(StorefrontPageCache::class)->invalidate('channel', 'home', 'studio');
+        app(SitemapCacheService::class)->invalidate();
     }
 
     public function updated(Studio $studio): void
