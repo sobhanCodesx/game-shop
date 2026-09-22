81faea6e44fb5f1273900df7aa812ab49415df1c
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-22 10:06:17 +0330

    Phase 2: invalidate channels on playlist changes

--- app/Observers/VideoPlaylistObserver.php
+++ app/Observers/VideoPlaylistObserver.php
@@ -26,6 +26,6 @@ class VideoPlaylistObserver
 
     private function invalidate(): void
     {
-        app(StorefrontPageCache::class)->invalidate('playlist');
+        app(StorefrontPageCache::class)->invalidate('playlist', 'channel');
     }
 }
