53cb039afa29a58cbf87636dd922deb4b5462f99
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-22 10:06:09 +0330

    Phase 2: add channel cache invalidation observers

---
+++ app/Observers/SocialContentMediaObserver.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\SocialContentMedia;
+use App\Services\StorefrontPageCache;
+
+class SocialContentMediaObserver
+{
+    public function created(SocialContentMedia $media): void
+    {
+        $this->invalidate($media);
+    }
+
+    public function updated(SocialContentMedia $media): void
+    {
+        if ($media->wasChanged(['social_content_id', 'type', 'path', 'thumbnail', 'mime', 'width', 'height', 'duration', 'alt', 'sort_order'])) {
+            $this->invalidate($media);
+        }
+    }
+
+    public function deleted(SocialContentMedia $media): void
+    {
+        $this->invalidate($media);
+    }
+
+    private function invalidate(SocialContentMedia $media): void
+    {
+        $type = $media->content()->value('type');
+
+        if ($type === 'video') {
+            app(StorefrontPageCache::class)->invalidate('playlist', 'channel');
+        } elseif ($type === 'post') {
+            app(StorefrontPageCache::class)->invalidate('channel');
+        }
+    }
+}
